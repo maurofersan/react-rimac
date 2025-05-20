f04fa9be5db59c540ef5f9d31e5c99ca4e723969
Mauro <mauro_9_2@hotmail.com>
2025-05-20 17:08:15 -0500

    modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import "./App.css";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { AppRouter } from "./router/AppRouter";
 import { GlobalProvider } from "./store/context/GlobalProvider";
 
 const App = () => {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <GlobalProvider>
         <AppRouter />
       </GlobalProvider>
-    </HashRouter>
+    </BrowserRouter>
   );
 };
 
