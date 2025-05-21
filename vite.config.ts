c024ecf3439cae44a3c091c1a16448673c2d959d
Mauro <mauro_9_2@hotmail.com>
2025-05-21 03:43:04 -0500

    added test for user page

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
+import { defineConfig as testConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
 // https://vite.dev/config/
-export default defineConfig({
+export const config = defineConfig({
   plugins: [react()],
   base: "/react-rimac",
   resolve: {
@@ -12,3 +13,16 @@ export default defineConfig({
     },
   },
 });
+
+export const tstConfig = testConfig({
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: "./src/test/setup.ts",
+  },
+});
+
+export default {
+  ...config,
+  ...tstConfig,
+};
